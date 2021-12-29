451b2d8627b80b5f6ff922cdd74436788dd75040
gabit <gabit690@gmail.com>
2021-12-29 10:29:36 -0300

    cyclorama animations and data of charpters.json

--- scripts/multimedia.js
+++ scripts/multimedia.js
@@ -8,22 +8,34 @@ const addAudioToBody = (audioPath) => {
     audio.play();
 };
 
+const setCycloramaView = (cyclorama, repetition) => {
+    setTimeout(() => {
+        cyclorama.classList.add(`show-cyclorama-${repetition}`);
+    }, 2000);
+};
+
 const setGoDownDrape = (mainDrape) => {
     setTimeout(() => {
         mainDrape.classList.remove("lift-up-billboard");
         mainDrape.classList.add("go-down-drape");
     }, 54000);
-}
+};
 
 window.addEventListener('DOMContentLoaded', () => {
     const billboardButton = document.getElementById("billboard");
     billboardButton.addEventListener("click", () => {
         if (billboardButton.classList.length === 0) {
-            const mainDrape = document.getElementById("main-drape");
             billboardButton.classList.add("lift-up-billboard");
-            mainDrape.classList.add("lift-up-drape");
-            addAudioToBody("./audio/smb3.mp3");
-            setGoDownDrape(mainDrape);
+            fetch("../data/chapters.json")
+                .then(response => response.json())
+                .then(data => {
+                    const mainDrape = document.getElementById("main-drape");
+                    const cyclorama = document.getElementById("cyclorama");
+                    mainDrape.classList.add("lift-up-drape");
+                    addAudioToBody("./audio/smb3.mp3");
+                    setCycloramaView(cyclorama, data.length);
+                    setGoDownDrape(mainDrape);
+                });
         }
     });
 });
