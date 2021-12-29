3027f3a7617ceaaa2ec187a833f1e09e038f8ae2
gabit <gabit690@gmail.com>
2021-12-28 23:54:22 -0300

    Animation and functionality of billboard and main drape

---
+++ scripts/billboard.js
@@ -0,0 +1,29 @@
+const addAudioToBody = (audioPath) => {
+    const audio = document.createElement("audio");
+    audio.setAttribute("src", audioPath);
+    document.body.appendChild(audio);
+    audio.addEventListener("ended", () => {
+        document.body.removeChild(audio);
+    });
+    audio.play();
+};
+
+const setGoDownDrape = (mainDrape) => {
+    setTimeout(() => {
+        mainDrape.classList.remove("lift-up-billboard");
+        mainDrape.classList.add("go-down-drape");
+    }, 54000);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const billboardButton = document.getElementById("billboard");
+    const mainDrape = document.getElementById("main-drape");
+    billboardButton.addEventListener("click", () => {
+        if (billboardButton.classList.length === 0) {
+            billboardButton.classList.add("lift-up-billboard");
+            mainDrape.classList.add("lift-up-drape");
+            addAudioToBody("./audio/smb3.mp3");
+            setGoDownDrape(mainDrape);
+        }
+    });
+});
